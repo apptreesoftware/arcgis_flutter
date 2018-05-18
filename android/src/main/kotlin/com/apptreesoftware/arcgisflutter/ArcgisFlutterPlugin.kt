3e0fc802a60e23cfcac062ed022ee453e44e1717
John Ryan <john.p.ryan4@gmail.com>
2018-05-18 12:17:06 -0700

    make app build

--- android/src/main/kotlin/com/apptreesoftware/arcgisflutter/ArcgisFlutterPlugin.kt
+++ android/src/main/kotlin/com/apptreesoftware/arcgisflutter/ArcgisFlutterPlugin.kt
@@ -1,25 +1,200 @@
 package com.apptreesoftware.arcgisflutter
 
+import android.app.Activity
+import android.content.Intent
+import android.location.Location
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import com.google.android.gms.maps.GoogleMap
 
-class ArcgisFlutterPlugin(): MethodCallHandler {
+class ArcgisFlutterPlugin(val activity: Activity) : MethodCallHandler {
+  val mapTypeMapping: HashMap<String, Int> = hashMapOf(
+          "none" to GoogleMap.MAP_TYPE_NONE,
+          "normal" to GoogleMap.MAP_TYPE_NORMAL,
+          "satellite" to GoogleMap.MAP_TYPE_SATELLITE,
+          "terrain" to GoogleMap.MAP_TYPE_TERRAIN,
+          "hybrid" to GoogleMap.MAP_TYPE_HYBRID
+  )
+
   companion object {
+    lateinit var channel: MethodChannel
+    var toolbarActions: List<ToolbarAction> = emptyList()
+    var showUserLocation: Boolean = false
+    var mapTitle : String = ""
+    lateinit var initialCameraPosition: CameraPosition
+    var mapActivity: MapActivity? = null
+    val REQUEST_GOOGLE_PLAY_SERVICES = 1000
+    var mapViewType: Int = GoogleMap.MAP_TYPE_NORMAL
+
     @JvmStatic
     fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "arcgis_flutter")
-      channel.setMethodCallHandler(ArcgisFlutterPlugin())
+      channel = MethodChannel(registrar.messenger(), "com.apptreesoftware.map_view")
+      val plugin = ArcgisFlutterPlugin(activity = registrar.activity())
+      channel.setMethodCallHandler(plugin)
+    }
+
+    fun handleToolbarAction(id: Int) {
+      channel.invokeMethod("onToolbarAction", id)
+    }
+
+    fun onMapReady() {
+      channel.invokeMethod("onMapReady", null)
+    }
+
+    fun getToolbarActions(actionsList: List<Map<String, Any>>?): List<ToolbarAction> {
+      if (actionsList == null) return emptyList()
+      val actions = ArrayList<ToolbarAction>()
+      actionsList.mapTo(actions) { ToolbarAction(it) }
+      return actions
+    }
+
+    fun getCameraPosition(map: Map<String, Any>): CameraPosition {
+      val latitude = map["latitude"] as Double
+      val longitude = map["longitude"] as Double
+      val zoom = map["zoom"] as Double
+      return CameraPosition(LatLng(latitude, longitude), zoom.toFloat(), 0.0f, 0.0f)
+    }
+
+    fun mapTapped(latLng: LatLng) {
+      this.channel.invokeMethod("mapTapped",
+              mapOf("latitude" to latLng.latitude,
+                      "longitude" to latLng.longitude))
+    }
+
+    fun annotationTapped(id: String) {
+      this.channel.invokeMethod("annotationTapped", id)
+    }
+
+    fun cameraPositionChanged(pos: CameraPosition) {
+      this.channel.invokeMethod("cameraPositionChanged", mapOf(
+              "latitude" to pos.target.latitude,
+              "longitude" to pos.target.longitude,
+              "zoom" to pos.zoom
+      ))
+    }
+
+    fun locationDidUpdate(loc: Location) {
+      this.channel.invokeMethod("locationUpdated", mapOf(
+              "latitude" to loc.latitude,
+              "longitude" to loc.longitude
+      ))
+    }
+
+    fun infoWindowTapped(id: String) {
+      this.channel.invokeMethod("infoWindowTapped", id)
     }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when {
+      call.method == "setApiKey" -> result.success(false)
+      call.method == "show" -> {
+        val code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(activity)
+        if (GoogleApiAvailability.getInstance().showErrorDialogFragment(activity, code, REQUEST_GOOGLE_PLAY_SERVICES)) {
+          return
+        }
+        val mapOptions = call.argument<Map<String, Any>>("mapOptions")
+        val cameraDict = mapOptions["cameraPosition"] as Map<String, Any>
+        initialCameraPosition = getCameraPosition(cameraDict)
+        toolbarActions = getToolbarActions(call.argument<List<Map<String, Any>>>("actions"))
+        showUserLocation = mapOptions["showUserLocation"] as Boolean
+        mapTitle = mapOptions["title"] as String
+
+        if (mapOptions["mapViewType"] != null) {
+          var mappedMapType: Int? = mapTypeMapping.get(mapOptions["mapViewType"]);
+          if (mappedMapType != null) mapViewType = mappedMapType as Int;
+        }
+
+        val intent = Intent(activity, MapActivity::class.java)
+        activity.startActivity(intent)
+        result.success(true)
+        return
+      }
+      call.method == "dismiss" -> {
+        mapActivity?.finish()
+        result.success(true)
+        return
+      }
+      call.method == "getZoomLevel" -> {
+        val zoom = mapActivity?.zoomLevel ?: 0.0.toFloat()
+        result.success(zoom)
+      }
+      call.method == "getCenter" -> {
+        val center = mapActivity?.target ?: LatLng(0.0, 0.0)
+        result.success(mapOf("latitude" to center.latitude,
+                "longitude" to center.longitude))
+      }
+      call.method == "setCamera" -> {
+        handleSetCamera(call.arguments as Map<String, Any>)
+        result.success(true)
+      }
+      call.method == "zoomToAnnotations" -> {
+        handleZoomToAnnotations(call.arguments as Map<String, Any>)
+        result.success(true)
+      }
+      call.method == "zoomToFit" -> {
+        mapActivity?.zoomToAnnotations(call.arguments as Int)
+        result.success(true)
+      }
+      call.method == "getVisibleMarkers" -> {
+        val visibleMarkerIds = mapActivity?.visibleMarkers ?: emptyList()
+        result.success(visibleMarkerIds)
+      }
+      call.method == "setAnnotations" -> {
+        handleSetAnnotations(call.arguments as List<Map<String, Any>>)
+        result.success(true)
+      }
+      call.method == "addAnnotation" -> {
+        handleAddAnnotation(call.arguments as Map<String, Any>)
+      }
+      call.method == "removeAnnotation" -> {
+        handleRemoveAnnotation(call.arguments as Map<String, Any>)
+      }
+      else -> result.notImplemented()
+    }
+  }
+
+  fun handleSetCamera(map: Map<String, Any>) {
+    val lat = map["latitude"] as Double
+    val lng = map["longitude"] as Double
+    val zoom = map["zoom"] as Double
+    mapActivity?.setCamera(LatLng(lat, lng), zoom.toFloat())
+  }
+
+  fun handleZoomToAnnotations(map: Map<String, Any>) {
+    val ids = map["annotations"] as List<String>
+    val padding = map["padding"] as Double
+    mapActivity?.zoomTo(annoationIds = ids, padding = padding.toFloat())
+  }
+
+  fun handleSetAnnotations(annotations: List<Map<String, Any>>) {
+    val mapAnnoations = ArrayList<MapAnnotation>()
+    for (a in annotations) {
+      val mapAnnotation = MapAnnotation.fromMap(a)
+      if (mapAnnotation != null) {
+        mapAnnoations.add(mapAnnotation)
+      }
+    }
+    mapActivity?.setAnnotations(mapAnnoations)
+  }
+
+  fun handleAddAnnotation(map: Map<String, Any>) {
+    MapAnnotation.fromMap(map)?.let {
+      mapActivity?.addMarker(it)
+    }
+  }
+
+  fun handleRemoveAnnotation(map: Map<String, Any>) {
+    MapAnnotation.fromMap(map)?.let {
+      mapActivity?.removeMarker(it)
     }
   }
 }
+
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:arcgis_flutter/arcgis_flutter.dart';
+import 'dart:async';
 
-void main() => runApp(new MyApp());
+import 'package:flutter/material.dart';
+import 'package:arcgis_flutter/map_view.dart';
+
+///This API Key will be used for both the interactive maps as well as the static maps.
+///Make sure that you have enabled the following APIs in the Google API Console (https://console.developers.google.com/apis)
+/// - Static Maps API
+/// - Android Maps API
+/// - iOS Maps API
+var API_KEY = "AIzaSyCzGs0MnqCcztKOIdxh8MEGDhUJflJ96Vo";
+
+void main() {
+  MapView.setApiKey(API_KEY);
+  runApp(new MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -10,46 +21,167 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  MapView mapView = new MapView();
+  CameraPosition cameraPosition;
+  var compositeSubscription = new CompositeSubscription();
+  var staticMapProvider = new StaticMapProvider(API_KEY);
+  Uri staticMapUri;
+
+  List<Marker> _markers = <Marker>[
+    new Marker("1", "Work", 45.523970, -122.663081, color: Colors.blue),
+    new Marker("2", "Nossa Familia Coffee", 45.528788, -122.684633),
+  ];
 
   @override
   initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await ArcgisFlutter.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted)
-      return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    cameraPosition = new CameraPosition(Locations.portland, 2.0);
+    staticMapUri = staticMapProvider.getStaticUri(Locations.portland, 12,
+        width: 900, height: 400, mapType: StaticMapViewType.roadmap);
   }
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Plugin example app'),
-        ),
-        body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+          appBar: new AppBar(
+            title: new Text('Map View Example'),
+          ),
+          body: new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                height: 250.0,
+                child: new Stack(
+                  children: <Widget>[
+                    new Center(
+                        child: new Container(
+                          child: new Text(
+                            "You are supposed to see a map here.\n\nAPI Key is not valid.\n\n"
+                                "To view maps in the example application set the "
+                                "API_KEY variable in example/lib/main.dart. "
+                                "\n\nIf you have set an API Key but you still see this text "
+                                "make sure you have enabled all of the correct APIs "
+                                "in the Google API Console. See README for more detail.",
+                            textAlign: TextAlign.center,
+                          ),
+                          padding: const EdgeInsets.all(20.0),
+                        )),
+                    new InkWell(
+                      child: new Center(
+                        child: new Image.network(staticMapUri.toString()),
+                      ),
+                      onTap: showMap,
+                    )
+                  ],
+                ),
+              ),
+              new Container(
+                padding: new EdgeInsets.only(top: 10.0),
+                child: new Text(
+                  "Tap the map to interact",
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              new Container(
+                padding: new EdgeInsets.only(top: 25.0),
+                child: new Text(
+                    "Camera Position: \n\nLat: ${cameraPosition.center.latitude}\n\nLng:${cameraPosition.center.longitude}\n\nZoom: ${cameraPosition.zoom}"),
+              ),
+            ],
+          )),
     );
+  }
+
+  showMap() {
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            initialCameraPosition: new CameraPosition(
+                new Location(45.5235258, -122.6732493), 14.0),
+            title: "Recently Visited"),
+        toolbarActions: [new ToolbarAction("Close", 1)]);
+
+    var sub = mapView.onMapReady.listen((_) {
+      mapView.setMarkers(_markers);
+      mapView.addMarker(new Marker("3", "10 Barrel", 45.5259467, -122.687747,
+          color: Colors.purple));
+      mapView.zoomToFit(padding: 100);
+    });
+    compositeSubscription.add(sub);
+
+    sub = mapView.onLocationUpdated
+        .listen((location) => print("Location updated $location"));
+    compositeSubscription.add(sub);
+
+    sub = mapView.onTouchAnnotation
+        .listen((annotation) => print("annotation tapped"));
+    compositeSubscription.add(sub);
+
+    sub = mapView.onMapTapped
+        .listen((location) => print("Touched location $location"));
+    compositeSubscription.add(sub);
+
+    sub = mapView.onCameraChanged.listen((cameraPosition) =>
+        this.setState(() => this.cameraPosition = cameraPosition));
+    compositeSubscription.add(sub);
+
+    sub = mapView.onToolbarAction.listen((id) {
+      if (id == 1) {
+        _handleDismiss();
+      }
+    });
+    compositeSubscription.add(sub);
+
+    sub = mapView.onInfoWindowTapped.listen((marker) {
+      print("Info Window Tapped for ${marker.title}");
+    });
+    compositeSubscription.add(sub);
+  }
+
+  _handleDismiss() async {
+    double zoomLevel = await mapView.zoomLevel;
+    Location centerLocation = await mapView.centerLocation;
+    List<Marker> visibleAnnotations = await mapView.visibleAnnotations;
+    print("Zoom Level: $zoomLevel");
+    print("Center: $centerLocation");
+    print("Visible Annotation Count: ${visibleAnnotations.length}");
+    var uri = await staticMapProvider.getImageUriFromMap(mapView,
+        width: 900, height: 400);
+    setState(() => staticMapUri = uri);
+    mapView.dismiss();
+    compositeSubscription.cancel();
+  }
+}
+
+class CompositeSubscription {
+  Set<StreamSubscription<dynamic>> _subscriptions = new Set();
+
+  void cancel() {
+    for (var n in this._subscriptions) {
+      n.cancel();
+    }
+    this._subscriptions = new Set();
+  }
+
+  void add(StreamSubscription subscription) {
+    this._subscriptions.add(subscription);
+  }
+
+  void addAll(Iterable<StreamSubscription> subs) {
+    _subscriptions.addAll(subs);
+  }
+
+  bool remove(StreamSubscription subscription) {
+    return this._subscriptions.remove(subscription);
+  }
+
+  bool contains(StreamSubscription subscription) {
+    return this._subscriptions.contains(subscription);
+  }
+
+  List<StreamSubscription> toList() {
+    return this._subscriptions.toList();
   }
 }
