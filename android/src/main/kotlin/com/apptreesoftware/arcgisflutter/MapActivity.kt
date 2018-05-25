b53adf3a30d752e799e3d3dc046ade06dfd9f35e
John Ryan <john.p.ryan4@gmail.com>
2018-05-25 15:48:05 -0700

    fix kotlin bug

--- android/src/main/kotlin/com/apptreesoftware/arcgisflutter/MapActivity.kt
+++ android/src/main/kotlin/com/apptreesoftware/arcgisflutter/MapActivity.kt
@@ -28,7 +28,6 @@ class MapActivity : AppCompatActivity(), LayersDialogFragment.LayersDialogListen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = ArcgisFlutterPlugin.mapTitle
 
         mapView = findViewById(R.id.mapView)
         mapView.graphicsOverlays.add(overlay)
